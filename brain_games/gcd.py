f2c465761542161ef18b39a88b5935039d417e72
Dmitriy Parfimovich <dparf@mail.ru>
2022-03-30 22:56:49 +0300

    add game get gcd

---
+++ brain_games/gcd.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from brain_games.cli import welcome_user
+from random import randint
+
+
+# getting an answer from user-------------------------------------------
+def get_answer():
+    global answer, x
+    x = randint(1, 99)
+    y = randint(1, 99)
+    answer = ''
+    print(f"Question: {x} {y}")
+    if x == y:
+        return x
+    while x != y:
+        if x > y:
+            x = x - y
+        if y > x:
+            y = y - x
+    while answer == '':
+        print('Your answer: ', end='')
+        answer = input()
+    return answer, x
+
+
+# execution of the correct answer from user-----------------------------
+def execute_correct_answer():
+    global x, answer, break_out_flag, s
+    break_out_flag = False
+    if answer == str(x):
+        print('Correct!')
+        break_out_flag = True
+        s += 1
+    return break_out_flag
+
+
+# execution of the wrong answer from user-------------------------------
+def execute_wrong_answer():
+    global x, answer
+    if answer != str(x):
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{x}'.")
+        return print("Let's try again, {}!".format(name))
+
+
+# brain-gcd game function----------------------------------------------
+def get_gcd():
+    global x, answer, name, break_out_flag, s
+    name = welcome_user()
+    s = 0
+    print('Find the greatest common divisor of given numbers.')
+    for i in range(3):
+        get_answer()
+        execute_correct_answer()
+        if break_out_flag is False:
+            execute_wrong_answer()
+            break
+    if s == 3:
+        print('Congratulations, {}!'.format(name))
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+from brain_games.gcd import get_gcd
+
+
+def main():
+    print("Welcome to the Brain Games!")
+
+# function from calc.py-------------------------------------------------
+    get_gcd()
+
+
+if __name__ == '__main__':
+    main()
